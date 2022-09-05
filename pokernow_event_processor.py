653bb073bd170b9582f3f8de9d97aa0445ce3b3e
Tao <speakmore@taos-mac-mini.lan>
2022-09-05 11:03:19 -0700

    x

--- pokernow_event_processor.py
+++ pokernow_event_processor.py
@@ -1,39 +1,9 @@
 #!/usr/bin/env python3
-"""Poker Game"""
+"""Process Pokernow Events"""
+import json
 import logging
-from enum import Enum
 
-class PokerGameState(Enum):
-    WAITING = 0
-    PREFLOP = 1
-    FLOP = 2
-    TURN = 3
-    RIVER = 4
-    FINISHED = 5
 
-class PokerGame:
-    def __init__(players):
-        self._players = players
-        self._state = PokerGameState.WAITING
-
-    def start_game():
-        self._state = PokerGameState.PREFLOP
-
-    def end_game():
-        self._state = PokerGameState.FINISHED
-
-    def next_state():
-        '''Return the next state.
-           If the game has not started or has finished, return -1.
-           Otherwise, return the next state in game.'''
-        if self._state in [PokerGameState.WAITING, PokerGameState.FINISHED]:
-            return -1
-        elif self._state == PokerGameState.PREFLOP:
-            return PokerGameState.FLOP
-        elif self._state == PokerGameState.FLOP:
-            return PokerGameState.TURN
-        elif self._state == PokerGameState.TURN:
-            return PokerGameState.RIVER
-        elif self._state == PokerGameState.RIVER:
-            return PokerGameState.PREFLOP
-
+def process(event, data):
+    logging.info('Event=' + event)
+    logging.info(json.dumps(data, indent=4))
