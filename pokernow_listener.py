96c0ec975c488300ef297b6b0495b4da04705169
Tao <speakmore@taos-mac-mini.lan>
2022-09-04 19:01:47 -0700

    x

--- pokernow_listener.py
+++ pokernow_listener.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-
+"""Listen Pokernow Game with SocketIO."""
 import argparse
+import json
+import os
 import logging
+import sys
 
 import socketio
 
@@ -11,48 +14,76 @@ def parse_args():
     parser.add_argument(
         '-gid',
         '--game-id',
-        dest="game_id",
+        dest='game_id',
         help='pokernow.club game id',
         nargs=1,
         required=True)
-    parser.add_argument(
-        '-n',
-        '--npt',
-        dest="npt",
-        default='',
-        help='pokernow.club npt cookie value (copy from browser)',
-        nargs=1)
-    parser.add_argument(
-        '-a',
-        '--apt',
-        dest="apt",
-        default='',
-        help='pokernow.club apt cookie value (copy from browser)',
-        nargs=1)
     args = parser.parse_args()
-    if not args.game and (not args.npt or not args.apt):
-        logging.error("You must a gameid and a npt/apt cookie value")
-        exit()
+    if not args.game_id:
+        logging.error('You must specify a game id!')
+        sys.exit()
     return args
 
 
-def get_cookie(apt, npt):
-    cookie_string = ''
-    cookie_string += f'apt={apt};' if apt else ''
-    cookie_string += f'npt={npt};' if npt else ''
-    return cookie_string
+def get_cookie():
+    npt = os.getenv('POKERNOW_NPT')
+    return f'npt={npt};'
 
 
 def start_server(game_id, cookie):
-    logging.info(f'args={args} cookie={cookie}')
+    logging.info(f'args={game_id} cookie={cookie}')
     socket_client = socketio.Client()
+
+    @socket_client.event
+    def connect():
+        logging.info('Connection has established.')
+
+    @socket_client.event
+    def connect_error(data):
+        logging.warning(json.dumps(data, indent=4))
+
+    @socket_client.event
+    def message(data):
+        logging.info(json.dumps(data, indent=4))
+
+    @socket_client.event
+    def disconnect():
+        logging.info('disconnected from server')
+
+    @socket_client.on('*')
+    def catch_all(event, data):
+        logging.info(event)
+        logging.info(json.dumps(data, indent=4))
+
+    url = f'https://www.pokernow.club/socket.io/?gameID={game_id}&firstConnection=true&EIO=3'
+    logging.info(f'Connecting to {url}')
+
+    socket_client.connect(
+        url,
+        wait=True,
+        wait_timeout=60,
+        transports='websocket',
+        headers={
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Connection': 'Upgrade',
+            'Host': 'www.pokernow.club',
+            'Origin': 'https://www.pokernow.club',
+            'Pragma': 'no-cache',
+            'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+            'Sec-WebSocket-Version': '13',
+            'Upgrade': 'websocket',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
+            'Cookie': cookie})
+    socket_client.wait()
 
 
 def main():
     args = parse_args()
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    start_server(args.game_id, get_cookie(args.apt, args.npt))
+    start_server(args.game_id[0], get_cookie())
 
 
 if __name__ == '__main__':
