bddf77a05fa812732ef9bf66d5964c47a6720dff
Tao <speakmore@taos-mac-mini.lan>
2023-03-10 18:58:10 -0800

    x

--- utils/openai_utils.py
+++ utils/openai_utils.py
@@ -5,7 +5,7 @@ def get_completion_response(prompt):
     openai.api_key = os.getenv('OPENAI_API_KEY')
     prompt += '\n\n###\n\n'
     completion = openai.Completion.create(
-        model='curie:ft-personal-2023-03-08-03-46-55',
+        model='curie:ft-personal-2023-03-11-02-46-24',
         prompt=prompt,
         temperature=0.2
     )
